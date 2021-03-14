5fbb508c6dc1e64a6ec405d708e900c5e0588f98
mikhail <reload2014@yandex.ru>
2021-03-14 22:19:05 +0300

    Initiation

--- test.c
+++ test.c
@@ -1,10 +1,48 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "bigint.h"
 
-int main(int argc, char *argv[]) {
-	struct bigint *X = bigint_construct_from_int(999999999);
-	struct bigint *Y = bigint_power_int(X, 5000);
-	bigint_print(Y);
-	return 0;
+int main(int argc, char *argv[]) {	
+	if (argc != 4) {
+		printf("Input bigint number, operation sign & another bigint!\n");
+	} else {
+		struct bigint *X = bigint_construct_from_int(atoll(argv[1]));
+		char *operation = argv[2];
+	
+		if (*operation == '^') {
+			int number = atoi(argv[3]);
+			struct bigint *result = bigint_power_int(X, number);
+			bigint_print(result);
+			return 0;
+		} else if (*operation == '+') {
+			struct bigint *Y = bigint_construct_from_int(atoi(argv[3]));
+			struct bigint *result = bigint_addition(X, Y);
+			bigint_print(result);
+			return 0;	
+		} else if (*operation == '-') {
+			struct bigint *Y = bigint_construct_from_int(atoi(argv[3]));
+			struct bigint *result = bigint_subtraction(X, Y);
+			bigint_print(result);
+			return 0;
+		} else if (*operation == '*') {
+			struct bigint *Y = bigint_construct_from_int(atoi(argv[3]));
+			struct bigint *result = bigint_multiplication(X, Y);
+			bigint_print(result);
+			return 0;
+		} else if (*operation == '/') {
+			struct bigint *Y = bigint_construct_from_int(atoi(argv[3]));
+			struct bigint *result = bigint_division(X, Y);
+			bigint_print(result);
+			return 0;
+		} else if (*operation == '%') {
+			struct bigint *Y = bigint_construct_from_int(atoi(argv[3]));
+			struct bigint *result = bigint_modulo(X, Y);
+			bigint_print(result);
+			return 0;
+		} else {
+			printf("Wrong operation!\n");
+			return 1;
+		}
+	}
 }
